2db44b2ac212cdab08165e186cc5789e199b1538
Guy <Guy@DANA-PC>
2018-01-10 18:37:43 +0200

    Added AnalogUltrasonic class.

---
+++ robot2018/util/AnalogUltrasonic.cpp
@@ -0,0 +1,17 @@
+/*
+ * AnalogUltrasonic.cpp
+ *
+ *  Created on: 10 בינו׳ 2018
+ *      Author: Guy
+ */
+
+#include "AnalogUltrasonic.h"
+
+namespace vulcan {
+
+AnalogUltrasonic::AnalogUltrasonic() {
+	// TODO Auto-generated constructor stub
+
+}
+
+} /* namespace vulcan */
---
+++ robot2018/util/AnalogUltrasonic.h
@@ -0,0 +1,20 @@
+/*
+ * AnalogUltrasonic.h
+ *
+ *  Created on: 10 בינו׳ 2018
+ *      Author: Guy
+ */
+
+#ifndef UTIL_ANALOGULTRASONIC_H_
+#define UTIL_ANALOGULTRASONIC_H_
+
+namespace vulcan {
+
+class AnalogUltrasonic {
+public:
+	AnalogUltrasonic();
+};
+
+} /* namespace vulcan */
+
+#endif /* UTIL_ANALOGULTRASONIC_H_ */
