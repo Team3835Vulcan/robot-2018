9cc5ccfc486e08a705d8b65f6ad816b1a73e3edf
Guy <Guy@DANA-PC>
2018-01-11 20:50:49 +0200

    Finished AnalogUltrasonic library.

--- robot2018/util/AnalogUltrasonic.cpp
+++ robot2018/util/AnalogUltrasonic.cpp
@@ -9,9 +9,27 @@
 
 namespace vulcan {
 
-AnalogUltrasonic::AnalogUltrasonic() {
-	// TODO Auto-generated constructor stub
+AnalogUltrasonic::AnalogUltrasonic(int channel, double aboveRange, double belowRange, double voltageToDistance)
+{
+	m_voltageToDistance = voltageToDistance;
+	m_aboveRange = aboveRange;
+	m_belowRange = belowRange;
+	analogChannel = new frc::AnalogInput(channel);
+}
 
+double AnalogUltrasonic::GetVoltage()
+{
+	return analogChannel->GetVoltage();
+}
+
+double AnalogUltrasonic::GetDistance()
+{
+	double distance = analogChannel->GetVoltage() * m_voltageToDistance;
+
+	if(distance > m_aboveRange || distance < m_belowRange)
+		return -1;
+
+	return distance;
 }
 
 } /* namespace vulcan */
--- robot2018/util/AnalogUltrasonic.h
+++ robot2018/util/AnalogUltrasonic.h
@@ -8,11 +8,21 @@
 #ifndef UTIL_ANALOGULTRASONIC_H_
 #define UTIL_ANALOGULTRASONIC_H_
 
+#include <AnalogInput.h>
+
 namespace vulcan {
 
 class AnalogUltrasonic {
 public:
-	AnalogUltrasonic();
+	AnalogUltrasonic(int channel, double m_belowRange, double m_aboveRange, double m_voltageToDistance);
+	double GetDistance();
+	double GetVoltage();
+
+private:
+	double m_voltageToDistance;
+	double m_aboveRange;
+	double m_belowRange;
+	frc::AnalogInput* analogChannel;
 };
 
 } /* namespace vulcan */
