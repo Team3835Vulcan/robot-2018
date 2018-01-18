e9a73c04a877ec9694876374cb0c222e1eb8d60e
Nadav Strahilevitz <nd.strahilevitz@gmail.com>
2018-01-18 20:38:15 +0200

    changed robotmap to constants, added instructions for file usage

---
+++ robot2018/src/Constants.h
@@ -0,0 +1,16 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#pragma once
+
+/*
+ * Add all needed constants here.
+ * Relevant constants are:
+ * 1. Ports on the robot(for speedcontrollers, sensors etc)
+ * 2. physical size measurements of the robot
+ * 3. physical limits of the robot
+ */
